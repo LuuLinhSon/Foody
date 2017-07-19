22d0a449391321952044ed736b292d7ed34f88d6
LinhSon <sonlinh249@gmail.com>
2017-07-19 17:21:32 +0700

    commit

--- app/src/main/java/com/project/luulinhson/foody/View/Login/LoginActivity.java
+++ app/src/main/java/com/project/luulinhson/foody/View/Login/LoginActivity.java
@@ -67,7 +67,6 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
         firebaseAuth = FirebaseAuth.getInstance();
         loginManager = LoginManager.getInstance();
-        firebaseAuth.signOut();
 
         callbackManager = CallbackManager.Factory.create();
 
--- app/src/main/java/com/project/luulinhson/foody/View/Main/MainActivity.java
+++ app/src/main/java/com/project/luulinhson/foody/View/Main/MainActivity.java
@@ -2,14 +2,54 @@ package com.project.luulinhson.foody.View.Main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.project.luulinhson.foody.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    LinearLayout lnMoiNhat,lnDanhMuc,lnKhuVuc;
+    TextView tvMoiNhat,tvDanhMuc,tvKhuVuc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lnMoiNhat = (LinearLayout) findViewById(R.id.lnMoiNhat);
+        lnDanhMuc = (LinearLayout) findViewById(R.id.lnDanhMuc);
+        lnKhuVuc = (LinearLayout) findViewById(R.id.lnKhuVuc);
+        tvMoiNhat = (TextView) findViewById(R.id.tvMoiNhat);
+        tvDanhMuc = (TextView) findViewById(R.id.tvDanhMuc);
+        tvKhuVuc = (TextView) findViewById(R.id.tvKhuVuc);
+
+        lnMoiNhat.setOnClickListener(this);
+        lnDanhMuc.setOnClickListener(this);
+        lnKhuVuc.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.lnMoiNhat:
+                tvMoiNhat.setTextColor(getResources().getColor(R.color.colorPrimary));
+                tvDanhMuc.setTextColor(getResources().getColor(R.color.colorBlack));
+                tvKhuVuc.setTextColor(getResources().getColor(R.color.colorBlack));
+                break;
+            case R.id.lnDanhMuc:
+                tvDanhMuc.setTextColor(getResources().getColor(R.color.colorPrimary));
+                tvMoiNhat.setTextColor(getResources().getColor(R.color.colorBlack));
+                tvKhuVuc.setTextColor(getResources().getColor(R.color.colorBlack));
+                break;
+            case R.id.lnKhuVuc:
+                tvKhuVuc.setTextColor(getResources().getColor(R.color.colorPrimary));
+                tvDanhMuc.setTextColor(getResources().getColor(R.color.colorBlack));
+                tvMoiNhat.setTextColor(getResources().getColor(R.color.colorBlack));
+                break;
+        }
     }
 }
